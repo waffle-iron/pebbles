3d64627535872bfee4cbb841eb05656c4299ad24
tourunen <olli.tourunen@csc.fi>
2015-10-20 09:56:17 +0300

    database export and restore scripts enhanced
    - added getopts based parsing
    - added -f flag for export file
    - added -t -option to specify table(s) to export
    - added -c -option for cleaning the old database file (it was the only behaviour previously)

--- scripts/export_database.bash
+++ scripts/export_database.bash
@@ -3,21 +3,41 @@
 # bail out on non-zero exit codes
 set -e
 
-# did we get a filename as an argument?
-if [ "xxx$1" == "xxx" ]; then
-  # nope, make one up
-  export_date=$(/bin/date --iso-8601=seconds)
-  export_file=db.sqlite.$export_date.gz
-else
-  export_file=$1
-fi
-
-if [ -e $export_file ]; then
-  echo "Export file $export_file already exists, remove it first"
-  exit 1
-fi
-
 DB_FILE=/webapps/pouta_blueprints/run/db.sqlite
 
+print_usage() {
+    echo "Usage: $0 [-f export_file] [-t table_pattern]"
+}
+
+# default export file name
+export_date=$(/bin/date --iso-8601=seconds)
+export_file="db.sqlite.$export_date.gz"
+
+table_pattern=""
+
+while getopts "h?t:f:" opt; do
+    case "$opt" in
+    h|\?)
+        print_usage
+        exit 0
+        ;;
+    t)  table_pattern=$OPTARG
+        ;;
+    f)  export_file=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ -e $export_file ]; then
+    echo "Export file $export_file already exists, remove it first"
+    exit 1
+fi
+
 echo "Exporting to $export_file"
-ssh www "echo '.dump' | sudo sqlite3 $DB_FILE" | gzip -c > $export_file
+if [ "xxx$table_pattern" != "xxx" ]; then
+    echo "match tables with pattern '$table_pattern'"
+fi
+
+ssh www "echo '.dump $table_pattern' | sudo sqlite3 $DB_FILE" | gzip -c > $export_file
--- scripts/restore_database.bash
+++ scripts/restore_database.bash
@@ -5,32 +5,54 @@ set -e
 
 DB_FILE=/webapps/pouta_blueprints/run/db.sqlite
 
-if [ "xxx$1" == "xxx" ]; then
-  echo "Usage: $0 <database_backup_file>"
-  exit 1
-fi
+print_usage() {
+    echo "Usage: $0 -f export_file [-c]"
+    echo "  -f <file> : export file to be loaded"
+    echo "  -c        : clear out the old database"
+}
+
+
+# default export file name
+export_date=$(/bin/date --iso-8601=seconds)
+export_file=db.sqlite.$export_date.gz
+
+while getopts "h?f:c" opt; do
+    case "$opt" in
+    h|\?)
+        print_usage
+        exit 0
+        ;;
+    f)  export_file=$OPTARG
+        ;;
+    c)  clean_database="1"
+    esac
+done
 
 backup_file=$1
 
-if [ ! -e $backup_file ]; then
-  echo "Backup file $backup_file not found"
-  exit 1
+if [ ! -e $export_file ]; then
+    echo "Backup file $backup_file not found"
+    exit 1
 fi
 
-current_date=$(/bin/date --iso-8601=seconds)
+if [ "xxx$clean_database" != "xxx" ]; then
+    current_date=$(/bin/date --iso-8601=seconds)
 
-echo "Stopping gunicorn http server workers"
-ssh www sudo supervisorctl stop gunicorn_app
+    echo "Stopping gunicorn http server workers"
+    ssh www sudo supervisorctl stop gunicorn_app
 
-echo "Creating a copy of old database file"
-ssh www sudo cp $DB_FILE $DB_FILE.$current_date
+    echo "Creating a copy of old database file"
+    ssh www sudo cp $DB_FILE $DB_FILE.$current_date
 
-echo "Truncating database"
-ssh www sudo truncate --size 0 $DB_FILE
+    echo "Truncating database"
+    ssh www sudo truncate --size 0 $DB_FILE
 
-echo "Loading backup $backup_file into database"
-zcat $backup_file | ssh www sudo sqlite3 $DB_FILE
+    echo "Loading export file $export_file into database"
+    zcat $export_file | ssh www sudo sqlite3 $DB_FILE
 
-echo "Starting gunicorn http server workers"
-ssh www sudo supervisorctl start gunicorn_app
-
+    echo "Starting gunicorn http server workers"
+    ssh www sudo supervisorctl start gunicorn_app
+else
+    echo "Loading  from $export_file"
+    zcat $export_file | grep "^INSERT INTO" | ssh www sudo sqlite3 $DB_FILE
+fi
